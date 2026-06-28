e2d3a888b876680edba64a5f8004c41792983808
Lukas Jost <lukas.jost@outlook.com>
2026-06-28 21:01:13 +0200

    feat: scaffold permissions service

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("gg.grounds.base-conventions") version "0.5.1"
+    id("io.quarkus") version "3.35.3"
+}
+
+kotlin { jvmToolchain(25) }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+
+tasks.withType<JavaCompile>().configureEach { options.release.set(25) }
+
+tasks.withType<KotlinCompile>().configureEach { compilerOptions.jvmTarget.set(JvmTarget.JVM_25) }
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/groundsgg/*")
+        credentials {
+            username = providers.gradleProperty("github.user").get()
+            password = providers.gradleProperty("github.token").get()
+        }
+    }
+}
+
+dependencies {
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.35.3"))
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-grpc")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("io.quarkus:quarkus-flyway")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-scheduler")
+    implementation("io.quarkus:quarkus-opentelemetry")
+
+    compileOnly("com.google.protobuf:protobuf-kotlin")
+
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.quarkus:quarkus-junit5-mockito")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
+}
